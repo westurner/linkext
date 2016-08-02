b8a5f494a1cd785837cd4492a67d05cc9dd51c0d
Wes Turner <wes@wrd.nu>
2016-08-02 03:21:46 -0500

    CLN,BUG: linkext-jquery.js: broken comments

--- linkext-jquery.js
+++ linkext-jquery.js
@@ -4,8 +4,8 @@
     /* 
     * - https://www.smashingmagazine.com/2010/05/make-your-own-bookmarklets-with-jquery/ 
     * - http://pastie.org/462639
-    * - http://www.codetoad.com/javascript_get_selected_text.html */
-    * */
+    * - http://www.codetoad.com/javascript_get_selected_text.html
+    */
 
 	// the minimum version of jQuery we want
 	var v = "3.1.0";
